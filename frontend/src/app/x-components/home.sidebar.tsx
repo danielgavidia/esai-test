fe911156d738216049de3eaad6fcd6590cde4e75
Daniel Gavidia <dgavidia1@gmail.com>
2024-12-17 09:35:20 -0500

    Fixed UI for sidebar

--- frontend/src/app/x-components/home.sidebar.tsx
+++ frontend/src/app/x-components/home.sidebar.tsx
@@ -20,28 +20,34 @@ const GlobalSidebar = () => {
   return (
     <Sidebar>
       {/* Logo header */}
-      <SidebarHeader className="flex items-start p-4">ESAI</SidebarHeader>
+      <SidebarHeader className="flex items-start p-4 bg-white">
+        <img
+          src="https://www.esai.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.4b6d8299.png&w=1080&q=75"
+          alt="ESAI"
+          className="h-14"
+        />
+      </SidebarHeader>
 
       {/* Sidebar Content */}
-      <SidebarContent>
+      <SidebarContent className="px-1 bg-white">
         {/* All ESAI Tools */}
         <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenuButton onClick={() => router.push("/")}>All ESAI Tools</SidebarMenuButton>
-          </SidebarGroupContent>
-        </SidebarGroup>
-
-        {/* History */}
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenuButton onClick={() => router.push("/history")}>History</SidebarMenuButton>
-          </SidebarGroupContent>
-        </SidebarGroup>
-
-        {/* Cards */}
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenuButton onClick={() => router.push("/cards")}>Cards</SidebarMenuButton>
+          <SidebarGroupContent className="space-y-1">
+            <SidebarMenuButton onClick={() => router.push("/")} className="font-semibold text-xs">
+              All ESAI Tools
+            </SidebarMenuButton>
+            <SidebarMenuButton
+              onClick={() => router.push("/history")}
+              className="font-semibold text-xs"
+            >
+              History
+            </SidebarMenuButton>
+            <SidebarMenuButton
+              onClick={() => router.push("/cards")}
+              className="font-semibold text-xs"
+            >
+              Cards
+            </SidebarMenuButton>
           </SidebarGroupContent>
         </SidebarGroup>
         <Separator />
