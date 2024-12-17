247fda7dc0004cf185600eec84b48116578d66d6
Daniel Gavidia <dgavidia1@gmail.com>
2024-12-16 19:57:13 -0500

    Added tools mock data

---
+++ frontend/src/app/x-components/tool.main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export interface ToolMainProps {
+  title: string;
+  tags: string[];
+  description: string;
+}
+
+const ToolMain = () => {
+  return <div>IntroSheet</div>;
+};
+
+export default ToolMain;
---
+++ frontend/src/app/x-components/tool.question-block.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuestionBlockCard = () => {
+  return <div>QuestionBlockCard</div>;
+};
+
+export default QuestionBlockCard;
