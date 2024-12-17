6bafcba6fb967e6d5374783c675ff83d369c63bb
Daniel Gavidia <dgavidia1@gmail.com>
2024-12-17 12:07:38 -0500

    Small changes to sidebar

--- frontend/src/app/x-components/home.sidebar.tsx
+++ frontend/src/app/x-components/home.sidebar.tsx
@@ -4,14 +4,12 @@ import { Separator } from "@/components/ui/separator";
 import {
   Sidebar,
   SidebarContent,
-  // SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
-  // SidebarGroupLabel,
   SidebarHeader,
-  // SidebarMenu,
   SidebarMenuButton,
 } from "@/components/ui/sidebar";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const GlobalSidebar = () => {
@@ -21,11 +19,13 @@ const GlobalSidebar = () => {
     <Sidebar>
       {/* Logo header */}
       <SidebarHeader className="flex items-start p-4 bg-white">
-        <img
-          src="https://www.esai.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.4b6d8299.png&w=1080&q=75"
-          alt="ESAI"
-          className="h-14"
-        />
+        <Link href={"/"}>
+          <img
+            src="https://www.esai.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.4b6d8299.png&w=1080&q=75"
+            alt="ESAI"
+            className="h-14"
+          />
+        </Link>
       </SidebarHeader>
 
       {/* Sidebar Content */}
